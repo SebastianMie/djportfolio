550387b478c5c73c2d74343e58d98209ccce3dc0
Sebastian Mierisch <sebastian.mierisch@gmail.com>
2023-08-28 10:39:40 +0200

    anpassungen api https

--- src/components/api.js
+++ src/components/api.js
@@ -1,9 +1,19 @@
 import axios from 'axios';
 
+let baseURL;
+
+if (window.location.hostname === "localhost") {
+  baseURL = 'http://localhost:3001';
+} else {
+  baseURL = (window.location.protocol === 'https:') ? 
+      'https://untildj.site:3001' : 
+      'http://untildj.site:3001';
+}
+
+
 const instance = axios.create({
-  baseURL: 'http://untildj.site:3001',
+  baseURL: baseURL,
   withCredentials: true
 });
-
 
 export default instance;
